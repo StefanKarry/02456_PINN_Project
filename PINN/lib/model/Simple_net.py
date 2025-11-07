5829541cdb04a7614f735470c1088b357cf7f93e
NinnaL <s203978@dtu.dk>
2025-11-07 11:04:07 +0100

    updated Simple_net

--- PINN/lib/model/Simple_net.py
+++ PINN/lib/model/Simple_net.py
@@ -7,21 +7,32 @@ class SimpleNet(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=7, padding='same'),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(64, 128, kernel_size=3, padding='same'),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(128, 256, kernel_size=3, padding='same'),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(256, 256, kernel_size=3, padding='same'),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(256, 64, kernel_size=3, padding='same'),
             nn.ReLU(),
-            nn.Conv2d(64, 1, kernel_size=3, padding='same'),
         )
 
-        self.fc = nn.Linear(1 * 28 * 28, 1)
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1 * 28 * 28, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 1)
+        )
 
     def forward(self, x):
         x = self.conv(x)
-        x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
